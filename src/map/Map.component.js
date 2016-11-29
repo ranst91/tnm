f7b3531f0faa193127cd6c08bf5b5151fa359094
ranst91 <ran.st@hotmail.com>
2016-11-29 17:31:41 +0200

    Comments & map methods added
    Some comments added and 2 ways of adding google maps to test each

--- src/map/Map.component.js
+++ src/map/Map.component.js
@@ -11,9 +11,15 @@ module.exports = {
 
         function initMap() {
             MapService.getMapData();
+
             MapService.map = new google.maps.Map(document.getElementById('map'));
         }
 
         google.maps.event.addDomListener(document.getElementById("map"), 'load', MapService.setMarkers(vm.markers));
     }
 };
+
+//https://maps.googleapis.com/maps/api/staticmap?markers
+
+// https://maps.googleapis.com/maps/api/staticmap?size=512x512&maptype=roadmap&markers=&key=YOUR_API_KEY
+// size:mid|color:red|San Francisco,CA|Oakland,CA|San Jose,CA - ENCODE this url with params
--- src/services/MapService.js
+++ src/services/MapService.js
@@ -22,6 +22,36 @@ module.exports = function ($http) {
     }
 
     function getMapData(){
-        $http.get('https://maps.googleapis.com/maps/api/js?key=***YOUR_API_KEY**&callback=initMap');
+        return $http.get(`https://maps.googleapis.com/maps/api/js?key=***YOUR_API_KEY**&callback=initMap`);
     }
 };
+
+// 1ST approach //
+//MapService --
+// function getMapData(data){
+//     data = encodeURI(data);
+//     return $http.get(`https://maps.googleapis.com/maps/api/staticmap?size=512x512&maptype=roadmap&markers=${data}&key=YOUR_API_KEY`);
+// }
+//MapComponent --
+// function initMap() {
+//     //Get data to pass to getmapdata
+//     MapService.getMapData(data).then(map => {
+//         vm.map = map;
+//     });
+//
+// }
+
+// 2ND approach //
+//MapService
+// function getMapData(){
+//     return $http.get(`https://maps.googleapis.com/maps/api/js?key=***YOUR_API_KEY**&callback=initMap`);
+// }
+//MapComponents
+// function initMap() {
+//     MapService.getMapData();
+//
+//     MapService.map = new google.maps.Map(document.getElementById('map'));
+// }
+//
+// google.maps.event.addDomListener(document.getElementById("map"), 'load', MapService.setMarkers(vm.markers));
+
